5845332b335c06dc5db89cb8eff50db7a5b31f6c
Bartosz Koniński <rharid2000@yahoo.com>
2013-04-22 20:22:01 +0200

    git-test

--- src/main/java/pl/miasi2013/spring/lab2/controller/MovieController.java
+++ src/main/java/pl/miasi2013/spring/lab2/controller/MovieController.java
@@ -1,5 +1,7 @@
 package pl.miasi2013.spring.lab2.controller;
 
+// git-test
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
