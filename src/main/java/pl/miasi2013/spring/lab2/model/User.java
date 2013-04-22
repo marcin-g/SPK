47a231bc90b1e4c041a243f803cae8fc191ef977
marcin-g <marcin.grzeskowiak@hotmail.com>
2013-04-22 20:49:07 +0200

    zmiana id

--- src/main/java/pl/miasi2013/spring/lab2/model/User.java
+++ src/main/java/pl/miasi2013/spring/lab2/model/User.java
@@ -55,11 +55,11 @@ public class User {
 		this.email = email;
 	}
 
-	private long getId() {
+	public long getId() {
 		return id;
 	}
 
-	private void setId(long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
