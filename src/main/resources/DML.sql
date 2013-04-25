f4d2761a23ced98d5e5270274d54c76017860065
Przemyslaw Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2013-04-25 19:46:36 +0200

    DDL

--- src/main/resources/DML.sql
+++ src/main/resources/DML.sql
@@ -1,6 +1,6 @@
 --insert into Movie(title) values('Bolek i Lolek - W piaskach Gobi');
 --insert into Movie(title) values('Bolek i Lolek - Lampa Aladyna');
 -- insert into Movie(title) values('Reksio - magik');
-MERGE INTO Movie USING (VALUES ('Reksio - magik'),('Bolek i Lolek - W piaskach Gobi'),('Bolek i Lolek - Lampa Aladyna')) 
-AS vals(y) ON Movie.title = vals.y
-WHEN NOT MATCHED THEN INSERT (TITLE) VALUES TRIM(vals.y);
+--MERGE INTO Movie USING (VALUES ('Reksio - magik'),('Bolek i Lolek - W piaskach Gobi'),('Bolek i Lolek - Lampa Aladyna')) 
+--AS vals(y) ON Movie.title = vals.y
+--WHEN NOT MATCHED THEN INSERT (TITLE) VALUES TRIM(vals.y);
