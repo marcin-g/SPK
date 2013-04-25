14019d35568b1bab1f7a0fd0370d2a26e19c55c3
Przemyslaw Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2013-04-25 20:01:03 +0200

    Ladowanie jednego usera + metoda getAllBooks

--- src/main/java/pl/miasi2013/spring/lab2/dao/BookRepository.java
+++ src/main/java/pl/miasi2013/spring/lab2/dao/BookRepository.java
@@ -1,12 +1,27 @@
 package pl.miasi2013.spring.lab2.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.lang.NotImplementedException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import pl.miasi2013.spring.lab2.model.Book;
+import pl.miasi2013.spring.lab2.model.Book.BookState;
+
+class BookMapper implements RowMapper<Book> {
+	@Override
+	public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return new Book(rs.getInt("ID"), rs.getString("TITLE"), rs.getString("ISBN"),
+				rs.getInt("year"), rs.getString("PUBLISHER"), rs.getString("REVIEW_URL"),
+				rs.getString("BOOK_URL"), BookState.valueOf(rs.getString("STATE"))) ;
+	}
+	
+}
 
 public class BookRepository implements BookRepositoryInterface {
 	
@@ -26,7 +41,8 @@ public class BookRepository implements BookRepositoryInterface {
 
 	@Override
 	public Collection<Book> getAllBooks() {
-		throw new NotImplementedException();
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		return jdbcTemplate.query("select * from Book", new BookMapper());
 	}
 
 	@Override
