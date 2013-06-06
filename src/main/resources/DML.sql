9a99b171ac190dd0e65e5a76f9cce6ee3b21bfb1
Przemyslaw Grzeszczak <przemyslaw.grzeszczak@gmail.com>
2013-06-07 00:46:50 +0200

    DML

--- src/main/resources/DML.sql
+++ src/main/resources/DML.sql
@@ -6,7 +6,10 @@ insert into Book(title, isbn, year, author, state) values('Pan Tadeusz', '12345'
 insert into Book(title, isbn, year, author, state) values('Pan Tadeusz: Początek', '12346', '1100', 'Adam Mickiewicz', 'AVAILABLE');
 insert into Book(title, isbn, year, author, state) values('Pan Tadeusz: Powrót Gerwazego', '12345', '1900', 'Adam Mickiewicz', 'AVAILABLE');
 insert into Book(title, isbn, year, author, state) values('Pan Tadeusz: Przewrót Gerwazego', '12345', '1900', 'Adam Mickiewicz', 'AVAILABLE');
+insert into Book(title, isbn, year, author, state) values('Pan Tadeusz: Nawrót Gerwazego', '12345', '1900', 'Adam Mickiewicz', 'AVAILABLE');
 
-insert into Borrow(book_id, user_id, begin) values(1, 1, 1370558512);
+insert into Borrow(book_id, user_id, begin, end) values(1, 1, 1370558512, 1370558522);
 insert into Borrow(book_id, user_id, begin) values(2, 1, 1370558512);
+insert into Borrow(book_id, user_id, begin, end) values(5, 1, 1370558512, 1370558532);
 insert into Borrow(book_id, user_id, begin) values(4, 2, 1370558512);
+insert into Borrow(book_id, user_id, begin, end) values(5, 2, 1370558533, 1370558534);
